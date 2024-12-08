047739f486a1e1b9560c9ff6434dfd361547cc6c
MSG-G <mouhagueye97@gmail.com>
2024-12-08 19:53:42 +0000

    Changement de nouveau

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("je suis un developpeur\n");
+        System.out.printf("hello, GitHub\n");
         }
     }
